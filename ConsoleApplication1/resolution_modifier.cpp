371ed264db5fa35e15997597feeab93e1eb20b32
BUPE <emirebp@gmail.com>
2020-02-02 00:05:43 -0600

    miscellaneous

--- ConsoleApplication1/resolution_modifier.cpp
+++ ConsoleApplication1/resolution_modifier.cpp
@@ -5,34 +5,25 @@
 using namespace std;
 using namespace cv;
 
+
 Mat test;
-
-
-void displayWindows(vector<Mat>& frames)
-{
-    for (int index = 0; index < frames.size(); ++index)
-    {
-        String windowTitle = "Window " + to_string(index);
-
-        imshow(windowTitle, frames[index]);
-    }
-}
-
+String path = "C:/Users/NVIDIA/Desktop/vision_por_computadora/Libreria de programas/imagen_reporte_resolucion/";
+String name = "Res_1.5_L";
+double resChange = 1.5;
 
 int main(int argc, char** argv)
 {
-	Mat matSrc = imread("../../gamma_correction/test.jpeg", IMREAD_ANYCOLOR);
+	Mat matSrc = imread("../../resolution_modifier/resolution_test.png", IMREAD_GRAYSCALE);
 	Mat matRes;
 
-   resize(matSrc, matRes, Size(),.5 , .5, INTER_CUBIC);
+   resize(matSrc, matRes, Size(), resChange, resChange, INTER_LINEAR);
 
    vector<Mat>images{ matSrc, matRes }; // and so forth
-
-   displayWindows(images, WINDOW_GUI_NORMAL);
-   imwrite("fixed.jpeg", matRes);
-
-	cout << "Press 'w' to quit..." << endl;
-	while (char(waitKey(1)) != 'w') {}
+   namedWindow("Imagen original", WINDOW_AUTOSIZE);
+   imshow("Imagen original", matSrc);
+   imshow("Res_1.5_NN", matRes);
+   imwrite(path + name +".png", matRes);
+	while (char(waitKey(1)) != 27) {}
 	
 	return 0;
 }
